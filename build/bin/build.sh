c340b244c45238300427a608138f763c249d5dda
Keegan Quinn <keeganquinn@gmail.com>
2018-01-23 06:46:11 -0800

    Clean up, simplify, update URLs.

--- build/bin/build.sh
+++ build/bin/build.sh
@@ -1,18 +1,11 @@
-#!/bin/bash
-
-# build an openwrt image suitable for uploading to a router
-
-#
-# Benjamin Foote
-# bfoote@bnf.net
-#
+#!/usr/bin/env bash
 
 function usage() {
     cat <<EOF
 
     usage:
 
-        $docker run -it ${IMAGE} architecture nodename
+        docker run -it ${IMAGE} architecture nodename
 
         this setup assumes you have a node registered with the ptp-api
 
@@ -23,60 +16,34 @@ function usage() {
         https://github.com/personaltelco/ptp-cab/tree/master/ptp-node/files
 
         ex: if your architecture is atheros
-        $docker run -it ${IMAGE} atheros nodename
-
-        $1 $2
+        docker run -it ${IMAGE} atheros nodename
 
 EOF
 
-    exit;
-
+    exit
 }
 
-if [ "" = "$1" ];
-then
-    usage;
-fi
+[ -z "$1" ] && usage
+
 
 ARCH=$1
 NODE=$2
 
-function foocab() {
-    cd /home/${OWUSER}/ptp-openwrt-files
-    perl FOOCAB.pl --node $NODE
-    mv output /home/${OWUSER}/openwrt/files
+set -e -x
 
-}
 
-function build_openwrt() {
-    cd /home/${OWUSER}/openwrt/
+cd /src/ptp-openwrt-files || exit 2
 
-    # set proper permissions for docker volumes
-    for DIR in bin build_dir dl
-    do
-        if [ -e $DIR ]
-        then
-            sudo chown -R ${OWUSER}:${OWUSER} $DIR
-            sudo chmod -R 775 $DIR
-        fi
-    done
+perl FOOCAB.pl --node "$NODE"
+mv output /src/lede/files
 
-    # copy architectur config
-    cp feeds/ptpcab/ptp-node/files/.config-${ARCH} ./.config
 
-    # make sure dowload directory stays here
-    sed -i 's/CONFIG_DOWNLOAD_FOLDER/#CONFIG_DOWNLOAD_FOLDER/g' .config
+cd /src/lede || exit 2
 
-    # config and build
-    make defconfig
-    time make BUILD_LOG=1 IGNORE_ERRORS=m V=99 -j17
+# copy architecture config; make sure dowload directory stays here
+sed 's/CONFIG_DOWNLOAD_FOLDER/#CONFIG_DOWNLOAD_FOLDER/g' \
+    "feeds/ptpcab/ptp-node/files/.config-${ARCH}" > .config
 
-    # look for build errors
-    for i in $(find logs -name compile.txt) ;
-    do
-        grep -H --label=$i 'Error ' $i grep -v ignored | awk -F: '{ print $1 }';
-    done
-}
-
-foocab
-build_openwrt
+# config and build
+make defconfig
+time make BUILD_LOG=1 FORCE_UNSAFE_CONFIGURE=1 IGNORE_ERRORS=m V=99
